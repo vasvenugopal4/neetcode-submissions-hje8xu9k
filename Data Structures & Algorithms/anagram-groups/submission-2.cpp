e85042d8e47f188a66280edbc746bc7ccac4ce13
Vasudevan Venugopal <vasu1999@gmail.com>
2026-05-17 18:38:31 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> charMap;
+        vector<vector<string>> ans;
+
+        for (int i = 0; i < strs.size(); i++) {
+            std::string key;
+            vector<int> charCount(26, 0);
+            for (char c : strs[i])
+                charCount[c - 'a']++;
+
+            for (int i : charCount)
+                key += std::to_string(i) + "-";
+
+            
+            charMap[key].push_back(strs[i]);
+        }
+
+        for (auto const& [key, val] : charMap)
+            ans.push_back(val);
+
+        return ans;
+    }
+};
